cd6d9f08f7225ca89357fe913d930fa223e2503f
Severin <43531943+Tarkin25@users.noreply.github.com>
2020-09-19 14:36:11 +0200

    put build into backend public folder

---
+++ usermanagement/backend/src/main/resources/public/precache-manifest.4f0a1c0367dbf41e7959d6795354fc43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7c2a78b9b837f324a67aa62bb0b840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e2d21524019d7a1d654",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "392eba7ed85beb1ac650",
+    "url": "/static/js/2.9e8ec4b4.chunk.js"
+  },
+  {
+    "revision": "81b8c2a1eb4e8b02ed7bc7c3519ac287",
+    "url": "/static/js/2.9e8ec4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e2d21524019d7a1d654",
+    "url": "/static/js/main.8770fe17.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
