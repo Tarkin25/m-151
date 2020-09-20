858d6bef670ef345da1eecca76c8757c64580439
Severin <43531943+Tarkin25@users.noreply.github.com>
2020-09-20 19:34:51 +0200

    added automatic browser opening

--- usermanagement/backend/src/main/java/ch/tbz/m151/usermanagement/UsermanagementApplication.java
+++ usermanagement/backend/src/main/java/ch/tbz/m151/usermanagement/UsermanagementApplication.java
@@ -1,15 +1,35 @@
 package ch.tbz.m151.usermanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.awt.*;
+import java.net.URL;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class UsermanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsermanagementApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner openBrowser() {
+		return args -> {
+				System.setProperty("java.awt.headless", "false");
+
+				if(Desktop.isDesktopSupported()) {
+					Desktop desktop = Desktop.getDesktop();
+
+					if(desktop.isSupported(Desktop.Action.BROWSE)) {
+						desktop.browse(new URL("http://localhost:8080").toURI());
+					}
+				}
+		};
 	}
 
 }
